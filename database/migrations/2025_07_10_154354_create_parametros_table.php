5956359a12e7a681ed1ff8a7ed98aa2aae0eacdf
AntonnyMalu <gabrielmalu15@gmail.com>
2025-07-10 12:04:07 -0400

    first commit

---
+++ database/migrations/2025_07_10_154354_create_parametros_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('parametros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->bigInteger('tabla_id')->nullable();
+            $table->text('valor')->nullable();
+            $table->text('rowquid')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('parametros');
+    }
+};
