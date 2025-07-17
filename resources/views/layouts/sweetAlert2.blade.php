f266f8420cb2a14271d6cb9cb01405bf0865cc21
Ing. Yonathan Castillo <leothan522@gmail.com>
2025-07-17 12:36:17 -0400

    Auth con bootstrap

---
+++ resources/views/layouts/sweetAlert2.blade.php
@@ -0,0 +1,11 @@
+@if(session()->has('sweetAlert2'))
+    <script type="application/javascript">
+        window.onload = function () {
+            SweetAlert.fire({
+                @foreach(session('sweetAlert2') as $key => $value)
+                {{ $key }}: @if(is_string(gettype($value))) '{{ $value }}' @else {{ $value ?? 'undefined' }}  @endif,
+                @endforeach
+            });
+        }
+    </script>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('inicio');
+})->name('web.index');
 
 Route::middleware([
     'auth:sanctum',
